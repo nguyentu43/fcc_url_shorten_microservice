ccf10048dc101484aaead48d600d8471b5b8b61c
Glitch (indecisive-maraca) <none>
2019-06-14 10:58:56 +0000

    😋💺 Checkpoint ./server.js:5024683/879 ./package.json:5024683/8

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 var cors = require('cors');
 
 var app = express();
+var dns = require('dns');
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
@@ -24,13 +25,28 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+const list = [];
+
+app.post("/api/shorturl/new", express.urlencoded(), function (req, res) {
+  const url = req.body.url;
+  dns.lookup(url, function(err, data){
+    if(err) res.json({ "error": "invalid URL" });
+    else
+    {
+      list.push(url);
+      res.json({"original_url": url, "short_url": list.length });
+    }
+  });
+  
 });
 
+app.get("/api/shorturl/:id", function(req, res){
+  const id = req.params.id;
+  res.redirect(list[id - 1]);
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
